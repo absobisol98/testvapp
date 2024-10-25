8b9f409887746f01041a0ae357d3cedb7787e53b
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-10-25 14:29:38 +0800

    Add volunteer resource

---
+++ app/Filament/Resources/VolunteerResource.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VolunteerResource\Pages;
+use App\Filament\Resources\VolunteerResource\RelationManagers;
+use App\Models\AffiliateType;
+use App\Models\User;
+use App\Models\Volunteer;
+use App\Settings\MailSettings;
+use Filament\Forms;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
+use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
+
+class VolunteerResource extends Resource
+{
+    protected static ?string $model = Volunteer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-bell';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Group::make()
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('media')
+                            ->hiddenLabel()
+                            ->avatar()
+                            ->collection('avatars')
+                            ->alignCenter()
+                            ->columnSpanFull(),
+
+                        Forms\Components\Actions::make([
+                            Action::make('resend_verification')
+                                ->label(__('resource.user.actions.resend_verification'))
+                                ->color('info')
+                                ->action(fn(MailSettings $settings, Model $record) => static::doResendEmailVerification($settings, $record)),
+                        ])
+                            // ->hidden(fn (User $user) => $user->email_verified_at != null)
+                            ->hiddenOn('create')
+                            ->fullWidth(),
+
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\TextInput::make('password')
+                                    ->password()
+                                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                                    ->dehydrated(fn(?string $state): bool => filled($state))
+                                    ->revealable()
+                                    ->required(),
+                                Forms\Components\TextInput::make('passwordConfirmation')
+                                    ->password()
+                                    ->dehydrateStateUsing(fn(string $state): string => Hash::make($state))
+                                    ->dehydrated(fn(?string $state): bool => filled($state))
+                                    ->revealable()
+                                    ->same('password')
+                                    ->required(),
+                            ])
+                            ->compact()
+                            ->hidden(fn(string $operation): bool => $operation === 'edit'),
+
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\Placeholder::make('email_verified_at')
+                                    ->label(__('resource.general.email_verified_at'))
+                                    ->content(fn(User $record): ?string => new HtmlString("$record->email_verified_at")),
+                                Forms\Components\Placeholder::make('created_at')
+                                    ->label(__('resource.general.created_at'))
+                                    ->content(fn(User $record): ?string => $record->created_at?->diffForHumans()),
+                                Forms\Components\Placeholder::make('updated_at')
+                                    ->label(__('resource.general.updated_at'))
+                                    ->content(fn(User $record): ?string => $record->updated_at?->diffForHumans()),
+                            ])
+                            ->compact()
+                            ->hidden(fn(string $operation): bool => $operation === 'create'),
+                    ])
+                    ->columnSpan(1),
+
+                Forms\Components\Tabs::make()
+                    ->schema([
+                        Forms\Components\Tabs\Tab::make('Details')
+                            ->icon('heroicon-o-information-circle')
+                            ->schema([
+                                Forms\Components\Grid::make(3)
+                                    ->schema([
+                                        Forms\Components\TextInput::make('firstname')
+                                            ->required()
+                                            ->maxLength(255),
+
+                                        Forms\Components\TextInput::make('middle_name')
+                                            ->maxLength(255),
+
+                                        Forms\Components\TextInput::make('lastname')
+                                            ->required()
+                                            ->maxLength(255),
+
+                                        Forms\Components\TextInput::make('username')
+                                            ->required()
+                                            ->maxLength(255)
+                                            ->live()
+                                            ->rules(function ($record) {
+                                                $userId = $record?->id;
+                                                return $userId
+                                                    ? ['unique:users,username,' . $userId]
+                                                    : ['unique:users,username'];
+                                            }),
+
+                                        Forms\Components\TextInput::make('email')
+                                            ->email()
+                                            ->required()
+                                            ->maxLength(255)
+                                            ->rules(function ($record) {
+                                                $userId = $record?->id;
+                                                return $userId
+                                                    ? ['unique:users,email,' . $userId]
+                                                    : ['unique:users,email'];
+                                            }),
+
+                                        Forms\Components\DatePicker::make('birthdate')
+                                            ->required(),
+                                    ]),
+
+                                Forms\Components\TextInput::make('school')->placeholder('(Optional)')->columnSpanFull(),
+
+                                Forms\Components\Fieldset::make('Company')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('company_name')->columnSpanFull(),
+
+                                        Forms\Components\TextInput::make('company_address')->label('Address')->columnSpanFull(),
+
+                                        Forms\Components\TextInput::make('company_representative')->label('Representative')->columnSpanFull(),
+
+                                        PhoneInput::make('company_contact_number')
+                                            ->label('Contact number'),
+
+                                        Forms\Components\TextInput::make('company_email')
+                                            ->email()
+                                            ->maxLength(255),
+                                    ]),
+
+                                Forms\Components\Fieldset::make('In Case of Emergency')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('emergency_contact_name')->label('Contact name'),
+
+                                        PhoneInput::make('emergency_contact_number')
+                                            ->label('Contact number'),
+                                    ]),
+
+
+                                Forms\Components\Select::make('affiliate_type_id')
+                                    ->columnSpanFull()
+                                    ->default(1)
+                                    ->label('')
+                                    ->options(AffiliateType::all()->pluck('name', 'id')->toArray()),
+
+                            ])
+                            ->columns(2),
+
+                    ])
+                    ->columnSpan([
+                        'sm' => 1,
+                        'lg' => 2
+                    ]),
+            ])
+            ->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                SpatieMediaLibraryImageColumn::make('media')->label('Avatar')
+                    ->collection('avatars')
+                    ->wrap(),
+                Tables\Columns\TextColumn::make('username')->label('Username')
+                    ->description(fn(Model $record) => $record->firstname . ' ' . $record->lastname)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('roles.name')->label('Role')
+                    ->formatStateUsing(fn($state): string => Str::headline($state))
+                    ->colors(['info'])
+                    ->badge(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')->label('Verified at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVolunteers::route('/'),
+            'create' => Pages\CreateVolunteer::route('/create'),
+            'edit' => Pages\EditVolunteer::route('/{record}/edit'),
+        ];
+    }
+}
