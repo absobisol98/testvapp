9f7088141011af6deeb9ca2e61254e31d578f135
harrisgurion888 <104294995+harrisgurion888@users.noreply.github.com>
2024-11-04 17:55:29 +0800

    homepage-header-footer changes
    - Changed the font sizes of the Landing Page
    - Changed the button sizes of the Landing Page
    - Changed the Logo size of the Landing Page
    - Added the tabs of Opportunity Lists and Calendar
    - Changed the fonts, logos, dropdown, and buttons sizes of Navbar and Footer

--- resources/views/custom/includes/footer.blade.php
+++ resources/views/custom/includes/footer.blade.php
@@ -1,15 +1,15 @@
-<div id="footer" class="py-[150px] px-[80px] flex items-center justify-center bg-[#F2F2F2]">
-    <div class="w-full flex items-center justify-start gap-[200px]">
-        <div class="w-[380px] flex flex-col items-start justify-between gap-[40px] text-[16px] font-[400]">
-            <img class="w-[380px]" src="{{ asset('img/logo-colored.png') }}" alt="">
+<div id="footer" class="py-[100px] px-[80px] flex items-center justify-center bg-[#F2F2F2]">
+    <div class="w-full flex items-center justify-start gap-[100px]">
+        <div class="w-[280px] flex flex-col items-start justify-between gap-[20px] text-[14px] font-[400]">
+            <img class="w-[280px]" src="{{ asset('img/logo-colored.png') }}" alt="">
 
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
 
             <p>All Rights Reserved | Ayala Foundation</p>
         </div>
 
-        <div class="w-full max-w-[500px] grid grid-cols-2">
-            <div class="col-span-1 flex flex-col items-start justify-center gap-1 text-[24px] font-[400]">
+        <div class="w-full max-w-[400px] grid grid-cols-2 gap-[10px]">
+            <div class="col-span-1 flex flex-col items-start justify-center gap-1 text-[20px] font-[400]">
                 <div class="w-full">
                     <a class="hover:underline" href="">Menu Link 1</a>
                 </div>
@@ -39,7 +39,7 @@
                 </div>
             </div>
 
-            <div class="col-span-1 flex flex-col items-start justify-center gap-1 text-[24px] font-[400]">
+            <div class="col-span-1 flex flex-col items-start justify-center gap-1 text-[20px] font-[400]">
                 <div class="w-full">
                     <a class="hover:underline" href="">Menu Link 1</a>
                 </div>
