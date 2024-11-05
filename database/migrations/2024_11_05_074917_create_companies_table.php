5a51c80689aae6562aa28a2086c0a208833e0df8
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-05 16:10:11 +0800

    migrations for adding data

---
+++ database/migrations/2024_11_05_074917_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name', 100);
+            $table->integer('cluster_id')->nullable()->index('cluster_id');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
