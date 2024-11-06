2dce14d0e3c082d1fdf38b30b87ac881532a3088
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-06 21:23:37 +0800

    programs

---
+++ app/Filament/Resources/ProgramResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProgramResource\Pages;
+use App\Filament\Resources\ProgramResource\RelationManagers;
+use App\Models\Program;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProgramResource extends Resource
+{
+    protected static ?string $model = Program::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-calendar';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManagePrograms::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ProgramResource/Pages/ManagePrograms.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ProgramResource\Pages;
+
+use App\Filament\Resources\ProgramResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManagePrograms extends ManageRecords
+{
+    protected static string $resource = ProgramResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
