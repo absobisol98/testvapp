2e4110c1023ab014e8e0afb96ae49193ae6ce1ca
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-08 01:01:06 +0800

    Policies and permission for superadmin (cluster,company,program,volunteer)

---
+++ database/migrations/2024_11_07_163946_add_permission_for_super_admin_company.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use BezhanSalleh\FilamentShield\Support\Utils;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $role = Utils::getRoleModel()::where('name', 'super_admin')->first();
+        $permissionPrefixes = Utils::getGeneralResourcePermissionPrefixes();
+
+        $permissions = collect();
+        $resource = 'company';
+
+        collect($permissionPrefixes)
+            ->each(function ($prefix) use ($resource, $permissions, $role) {
+                $permissions->push(Utils::getPermissionModel()::firstOrCreate([
+                    'name' => $prefix . '_' . $resource,
+                    'guard_name' => Utils::getFilamentAuthGuard()
+                ]));
+            });
+
+        $role->givePermissionTo($permissions);
+
+        app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
