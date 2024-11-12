19c3a7cb14972a0dbb26ec147d3a64adf67c3175
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-12 20:47:56 +0800

    migrations

---
+++ database/migrations/2024_11_12_091818_add_columns_to_events_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->unsignedInteger('event_parent_id')->nullable();
+            $table->foreign('event_parent_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+};
