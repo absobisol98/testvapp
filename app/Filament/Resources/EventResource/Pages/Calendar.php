a834b00a8f4e627f49c206a2f7238a277bc1e632
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-19 14:15:01 +0800

    Update Calendar.php

--- app/Filament/Resources/EventResource/Pages/Calendar.php
+++ app/Filament/Resources/EventResource/Pages/Calendar.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
+use Filament\Actions\Action;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\Page;
 
 class Calendar extends Page
@@ -10,4 +12,20 @@ class Calendar extends Page
     protected static string $resource = EventResource::class;
 
     protected static string $view = 'filament.resources.event-resource.pages.calendar';
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('List')
+                ->icon('heroicon-o-list-bullet')
+                ->url(route('filament.admin.resources.events.index')),
+            Action::make('Thumbnail')
+                ->icon('heroicon-o-photo')
+                ->url(route('filament.admin.resources.events.thumbnail')),
+            CreateAction::make()
+                ->label('Event')
+                ->icon('heroicon-o-plus')
+                ->url(route('filament.admin.resources.events.create')),
+        ];
+    }
 }
