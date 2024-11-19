6d522452809bb71cb24c1c2c446290f2bba4fa72
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-19 15:33:29 +0800

    Update VolunteerFields.php

--- app/Actions/VolunteerFields.php
+++ app/Actions/VolunteerFields.php
@@ -90,7 +90,7 @@ final class VolunteerFields
 
                     TextInput::make('company_address')->label('Address')->columnSpanFull(),
 
-                    TextInput::make('company_representative')->label('Representative')->columnSpanFull(),
+                    TextInput::make('company_representative')->label('HR Representative')->columnSpanFull(),
 
                     PhoneInput::make('company_contact_number')
                         ->label('Contact number'),
@@ -98,49 +98,57 @@ final class VolunteerFields
                     TextInput::make('company_email')
                         ->email()
                         ->maxLength(255),
+
+
+                    Select::make('affiliate_type_id')
+                        ->columnSpanFull()
+                        ->default(1)
+                        ->label('')
+                        ->live()
+                        ->options(AffiliateType::all()->pluck('name', 'id')->toArray()),
+
+                    Select::make('company_id')
+                        ->required()
+                        ->prefixIcon('heroicon-o-building-office')
+                        ->prefixIconColor('primary')
+                        ->visible(fn (Get $get) => $get('affiliate_type_id') != 3)
+                        ->columnSpanFull()
+                        ->label('')
+                        ->searchable()
+                        ->options(function($get){
+
+                            $options = [];
+
+                            $clusters = Cluster::with(['companies'])->get();
+
+                            if($get && $get('affiliate_type_id') == 1){
+                                $clusters = Cluster::with(['companies'])->where('id',1)->get();
+                            }else if($get && $get('affiliate_type_id') == 2){
+                                $clusters = Cluster::with(['companies'])->where('id','!=',1)->get();
+                            }
+
+                            foreach ($clusters as $cluster) {
+                                $options[$cluster->name] = collect($cluster->companies)->mapWithKeys(function ($company) {
+                                    return [$company->id => $company->name];
+                                })->toArray();
+                            }
+
+                            return $options;
+                        }),
                 ]),
 
             Fieldset::make('In Case of Emergency')
                 ->schema([
                     TextInput::make('emergency_contact_name')->label('Contact name'),
+                    TextInput::make('emergency_contact_relationship')->label(' Relationship'),
 
                     PhoneInput::make('emergency_contact_number')
                         ->label('Contact number'),
                 ]),
 
-
-            Select::make('affiliate_type_id')
-                ->columnSpanFull()
-                ->default(1)
-                ->label('')
-                ->live()
-                ->options(AffiliateType::all()->pluck('name', 'id')->toArray()),
-
-            Select::make('company_id')
-                ->required()
-                ->prefixIcon('heroicon-o-building-office')
-                ->prefixIconColor('primary')
-                ->visible(fn (Get $get) => $get('affiliate_type_id') == 1)
-                ->columnSpanFull()
-                ->label('')
-                ->searchable()
-                ->options(function(){
-
-                    $options = [];
-
-                    $clusters = Cluster::with(['companies'])->get();
-                    foreach ($clusters as $cluster) {
-                        $options[$cluster->name] = collect($cluster->companies)->mapWithKeys(function ($company) {
-                            return [$company->id => $company->name];
-                        })->toArray();
-                    }
-
-                    return $options;
-                }),
-
             Select::make('program_id')
                 ->columnSpanFull()
-                ->label('What programs are you interested in?')
+                ->label('Interests')
                 ->options(Program::all()->pluck('name', 'id')->toArray()),
         ];
 
