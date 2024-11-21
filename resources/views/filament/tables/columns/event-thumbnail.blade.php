bfbb1478fbf5496f88512e2fd5601587b0fb1ea4
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-11-21 16:16:18 +0800

    event display

--- resources/views/filament/tables/columns/event-thumbnail.blade.php
+++ resources/views/filament/tables/columns/event-thumbnail.blade.php
@@ -1,11 +1,20 @@
 @php
     $record = $getRecord();
 @endphp
-    <div class="rounded overflow-y-hidden h-[120px]">
+    <div class="rounded overflow-y-hidden h-[180px]">
         <div class="font-bold text-2xl mb-2 truncate event-title"  >{{$record->title}}</div>
         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{\Carbon\Carbon::parse($record->start_date)->format('F d, Y')}}</div>
         <p class="mt-2 text-primary-500 text-sm">{{\Carbon\Carbon::parse($record->start_date)->format('h:i A')}} - {{\Carbon\Carbon::parse($record->end_date)->format('h:i A')}}</p>
-        <div class="mt-2 !text-gray-500 truncate event-description">{!! $record->description !!}</div>
+        <div>
+            <b>Slot: </b>
+            @foreach($record->slots as $slot)
+                @php
+                    $registion_count = $record->registrations->where('slot_type_id',$slot->id)->where('status_id','!=',3)->count();
+                @endphp
+               <span class="text-sm"><b>({{$slot->total_slots - $registion_count}})</b> {{$slot->type->name}}: {{Carbon\Carbon::parse(now()->format('Y-m-d').$slot->start_time)->format('h:i')}} - {{Carbon\Carbon::parse(now()->format('Y-m-d').$slot->end_time)->format('h:i')}}</span>
+            @endforeach
+        </div>
+        <div class="mt-2 !text-gray-500 no-scrollbar overflow-y-scroll max-h-12 h-12">{!! $record->description !!}</div>
 
     </div>
 @push('scripts')
