ae94e38e854904177fe09befd71da430f6d8231e
joshraphaelorolfo <joshorolfo04@gmail.com>
2024-12-10 00:32:17 +0800

    Update ListVolunteers.php

--- app/Filament/Resources/VolunteerResource/Pages/ListVolunteers.php
+++ app/Filament/Resources/VolunteerResource/Pages/ListVolunteers.php
@@ -12,17 +12,6 @@ class ListVolunteers extends ListRecords
 {
     protected static string $resource = VolunteerResource::class;
 
-    protected function getTableQuery(): ?Builder
-    {
-        if(auth()->user()->hasRole(['super_admin'])){ // Super admin
-            $events = (new (static::$resource::getModel()));
-        }else{
-            $events = (new (static::$resource::getModel()))->where('id',auth()->id());
-        }
-
-        return $events;
-    }
-
     protected function getHeaderActions(): array
     {
         return [
