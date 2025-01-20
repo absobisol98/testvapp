b2f6c5490d9edf282187bc6a5b2ab11a1c08b4ba
Jay-Eclarinal <138432803+Jay-Eclarinal@users.noreply.github.com>
2025-01-20 13:51:08 +0800

    view-events
    plot view events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,6 +6,7 @@
 
 namespace App\Models;
 
+use App\Filament\Resources\EventResource\Pages\EventPage;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
